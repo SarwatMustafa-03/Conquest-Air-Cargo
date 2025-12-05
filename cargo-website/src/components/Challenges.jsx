9aadc5cfadc8986cee7fe1415bf34e38f97c8797
Sarwat Mustafa <sarwatmustafa03@gmail.com>
2025-12-05 18:52:30 +0500

    fix: adjust width of Challenges component for better responsiveness

--- cargo-website/src/components/Challenges.jsx
+++ cargo-website/src/components/Challenges.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Challenges = () => {
   return (
-    <div className="max-w-7xl h-90 mx-auto p-4 sm:p-8 md:p-12 lg:p-16 bg-gradient-to-br from-slate-800 via-slate-700 to-amber-900">
+    <div className="max-w-full h-90 mx-auto p-4 sm:p-8 md:p-12 lg:p-16 bg-gradient-to-br from-slate-800 via-slate-700 to-amber-900">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 sm:gap-6 md:gap-8">
         
         {/* Target Audience Section */}
